bb98341bd1480652f7a90085aad04b260fde13e2
Harsh Kumar <harshks2320@gmail.com>
2025-11-26 02:57:37 +0530

    Logger: Libraries added

---
+++ src/logger/__init__.py
@@ -0,0 +1,5 @@
+import logging     # Python's built in logging framework (to print logs like DEBUG, INFO, ERRORS, etc)
+import os   # For working with folders and paths. 
+from datetime import datetime   # To work with date and time
+from logging.handlers import RotatingFileHandler    # Special handler that automatically rotates files when they grow too big.
+
