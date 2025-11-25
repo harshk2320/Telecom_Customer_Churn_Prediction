020d5b06eb85e198bc0a3ccc830d9c55efa99727
Harsh Kumar <harshks2320@gmail.com>
2025-11-26 03:28:32 +0530

    Logger: Added constants for logging config.

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -3,3 +3,14 @@ import os   # For working with folders and paths.
 from datetime import datetime   # To work with date and time
 from logging.handlers import RotatingFileHandler    # Special handler that automatically rotates files when they grow too big.
 
+
+
+LOG_DIR = "log"     # Name of the folder where the logs will be saved.
+
+LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"    # Generates log files based on current timestamp.
+# Timestamp is used to prevent overwrite of previous logs. Every run gets a fresh file  
+
+MAX_LOG_SIZE = 5 * 1024 * 1024   # Maximum file size 5 MB. When the file exceeds this size, it rotates (auto creates a new file).  
+
+BACKUP_COUNT = 3    # Keeps only 3 old log files.
+
