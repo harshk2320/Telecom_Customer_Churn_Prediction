d324528eb8c958219f0bea16839464c5941a6fe9
Harsh Kumar <harshks2320@gmail.com>
2025-11-26 22:08:20 +0530

    Logger: Added Configure_logger(). Checked if logger is working fine. Works well.

---
+++ demo.py
@@ -0,0 +1,8 @@
+## Below code is to check logging configuration
+from src.logger import logging
+
+logging.debug("This is a debug message")
+logging.debug("This is an info message")
+logging.warning("This is an warning message")
+logging.error("This is an error message")
+logging.critical("This is an Critical message")
--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -3,12 +3,12 @@ import logging     # Python's built in logging framework (to print logs like DEB
 import os   # For working with folders and paths. 
 from datetime import datetime   # To work with date and time
 from logging.handlers import RotatingFileHandler    # Special handler that automatically rotates files when they grow too big.
-
+# from from_root import from_root 
 
 ## Logging config constants
-LOG_DIR = "log"     # Name of the folder where the logs will be saved.
+LOG_DIR = "logs"     # Name of the folder where the logs will be saved.
 
-LOG_FILE = f"{datetime.now().strftime("%m_%d_%Y_%H_%M_%S")}.log"    # Generates log files based on current timestamp.
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"    # Generates log files based on current timestamp.
 # Timestamp is used to prevent overwrite of previous logs. Every run gets a fresh file  
 
 MAX_LOG_SIZE = 5 * 1024 * 1024   # Maximum file size 5 MB. When the file exceeds this size, it rotates (auto creates a new file).  
@@ -18,7 +18,7 @@ BACKUP_COUNT = 3    # Keeps only 3 old log files.
 
 
 ## Constructing log file path
-log_dir_path = os.path.join(os.getcwd, LOG_DIR)    # Path of the log directory.
+log_dir_path = os.path.join(os.getcwd(), LOG_DIR)    # Path of the log directory.
 
 os.makedirs(log_dir_path, exist_ok= True)   # Creates log folder if does not already exist.
 
@@ -26,3 +26,40 @@ log_file_path = os.path.join(log_dir_path, LOG_FILE)    # Path to log file.
 
 
 
+def configure_logger():
+    """
+    This function is responsible for setting up the whole logging system for my project.
+    It ensures that logs are stored in a structured format & are stored in file handler & console handler. 
+    """
+    
+    logger = logging.getLogger()    
+    # A logger is a manager that controls when, where and how the log messages are printed.
+    # logging.getLogger() creates a main logging object.
+
+    logger.setLevel(logging.DEBUG)
+    # We basically set level as DEBUG, since its the lowest level hence allowing all types of logs.
+     
+    formatter = logging.Formatter("[%(asctime)s]%(name)s - %(levelname)s - %(message)s")
+    # This basically decides how the log message would look like. It makes the logs neat, readable, & professional.
+
+
+    ## File Handler
+    # It sends logs to file. Here we will be using RotatingFileHandler which automatically creates a new log file when one becomes too big.
+    file_handler = RotatingFileHandler(filename= log_file_path, maxBytes= MAX_LOG_SIZE, backupCount= BACKUP_COUNT)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.DEBUG)    # File stores all the logs from DEBUG to CRITICAL.
+
+    
+    ## Console Handler
+    # It shows log in terminal/ screen.
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.INFO)   # We donts want debug messages in the console since they are too big and noisy. We want only important messages.
+
+    ## Adding handler to the logger
+    # Now we attach both the handlers to the logger.
+    logger.addHandler(file_handler)     # All the messages appear in the file including deep debugging info.
+    logger.addHandler(console_handler)    # Only important messages appear in the terminal 
+
+                
+configure_logger()    # Calling the logger config function
