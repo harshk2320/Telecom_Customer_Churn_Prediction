49cc05bd5e65a6588f13aefffa6afb11e3fac0ba
Harsh Kumar <harshks2320@gmail.com>
2025-11-30 14:02:04 +0530

    Constants: Added General pipeline settings constants.

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -2,10 +2,14 @@ import os
 from datetime import date
 
 
-# For MongoDB Connection
+# MongoDB Connection/ Configuration
 DATABASE_NAME = "Proj3"     # Name of MongDB Database. 
 COLLECTION_NAME = "Proj3-Data"  # Name of the collection inside the DB where data is stored.
 MONGODB_URL_KEY = "MONGODB_URL"  # The environment variable key for MongoDB URL.
+
+# General Pipeline Setting
+PIPELINE_NAME: str = ""     # Optional naming for your ML pipeline (empty for now).
+ARTIFACT_DIR: str = "artifact"      # Folder where all outputs (cleaned data, models, report) are saved.
 
 
 
