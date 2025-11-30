3b8c2411a5c9a6ed2356e6d9092407afd4f74ca4
Harsh Kumar <harshks2320@gmail.com>
2025-11-30 13:57:20 +0530

    Constants: Added MongoDB Configuration/ Connection constants.

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -2,3 +2,11 @@ import os
 from datetime import date
 
 
+# For MongoDB Connection
+DATABASE_NAME = "Proj3"     # Name of MongDB Database. 
+COLLECTION_NAME = "Proj3-Data"  # Name of the collection inside the DB where data is stored.
+MONGODB_URL_KEY = "MONGODB_URL"  # The environment variable key for MongoDB URL.
+
+
+
+
