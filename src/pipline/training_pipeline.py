3da5c23c5868044fbe547131b843c01130e83521
Harsh Kumar <harshks2320@gmail.com>
2025-12-04 02:20:16 +0530

    training_pipeline.py: Added/ Updated Libraries DataIngestionConfig, DataIngestionArtifact.

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -4,4 +4,5 @@ from src.exception import MyException
 from src.logger import logging
 
 from src.components.data_ingestion import DataIngestion
- 
+from src.entity.config_entity import DataIngestionConfig
+from src.entity.artifact_entity import DataIngestionArtifact
