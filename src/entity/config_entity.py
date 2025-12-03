62f90413e35ee31a3ac59aa665d7f8ee831cc77b
Harsh Kumar <harshks2320@gmail.com>
2025-12-03 17:03:21 +0530

    config_entity.py: Added Class TrainingPipelineConfig.

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -22,3 +22,23 @@ from src.constants import *     # Contains global constants and file paths.
 from dataclasses import dataclass   # It automatically creates constructor such as (__init__(),     __repr__(), __eq__())
 from datetime import datetime   # Generate timestamps to create unique directories.
 
+TIMESTAMP = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")    # This generates unique folder name for the run.
+
+@dataclass
+# It is python decorator which automatically generates method (__init__(),__repr__(),__eq__() etc)
+# It is used for storing configurations without writing boiler plate codes
+
+class TrainingPipelineConfig:
+    """
+    This class stores high level configuration for ML Training Pipeline.
+    """
+
+    pipeline_name: str = PIPELINE_NAME      # Name of the pipelien  
+    artefacts: str = os.path.join(ARTIFACT_DIR, TIMESTAMP)    # Folder where my artifacts will be stored.
+    timestamp: str = TIMESTAMP     # Used to track when the run happened.
+
+    
+
+
+
+
