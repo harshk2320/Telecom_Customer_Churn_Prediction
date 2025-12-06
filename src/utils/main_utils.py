da9894aac32e860246a5549730a31b2a76caeb2d
Harsh Kumar <harshks2320@gmail.com>
2025-12-06 19:55:25 +0530

    main_utils.py: Added read_yaml_file function.

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -8,3 +8,18 @@ from pandas import DataFrame    # Convert to dataframe
 
 from src.exception import MyException   # Custom exception 
 from src.logger import logging    # To log message.
+
+
+def read_yaml_file(file_path: str) -> dict:
+    """
+    Opens a .yaml file. Reads its content into python dictionary, & then returns a dictionary.
+    
+    :param file_path: path to .yaml file
+    :return: dict
+    """
+    try:
+        with open(file_path, "rb") as yaml_file:
+            return yaml.safe_load(yaml_file)
+        
+    except Exception as e:
+        raise MyException(e, sys)
